410db6b4246fcf6cc43d2b31f4453ba8c3969a3c
the-daniel-rothig <daniel.rothig@gmail.com>
2019-11-02 00:47:10 +0000

    adopt standard monorepo folder structure

--- packages/yup-extensions/src/setDefaultLocale.js
+++ packages/yup-extensions/src/setDefaultLocale.js
@@ -8,7 +8,7 @@ export const localeValues = {
   },
   string: {
     max: ({label, max}) => max === 1 ? `${label} must be no more than ${max} character` : `${label} must be no more than ${max} characters`,
-    min: ({label, min}) => min === 1 ? `${label} must be at least ${max} character` : `${label} must be at least ${max} characters`,
+    min: ({label, min}) => min === 1 ? `${label} must be at least ${min} character` : `${label} must be at least ${min} characters`,
     email: "please enter a valid email address",
     mustNotContain: "${path} must not contain '${match}'"
   }
